42189400c03144261c0d54132e0cde7fa0f0b5b7
vincent <debimard@almacam.fr>
2019-02-01 10:55:59 +0100

    shortCut

--- AlmaCamShortcut/Form1.Designer.cs
+++ AlmaCamShortcut/Form1.Designer.cs
@@ -9,7 +9,7 @@
 
         /// <summary>
         /// Nettoyage des ressources utilisées.
-        /// </summary>
+        /// </s
         /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
         protected override void Dispose(bool disposing)
         {
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BT_open_AlmaCam = new System.Windows.Forms.Button();
+            this.LB_Server_List = new System.Windows.Forms.ComboBox();
+            this.BT_open_WPM = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BT_open_AlmaCam
+            // 
+            this.BT_open_AlmaCam.Location = new System.Drawing.Point(12, 39);
+            this.BT_open_AlmaCam.Name = "BT_open_AlmaCam";
+            this.BT_open_AlmaCam.Size = new System.Drawing.Size(503, 23);
+            this.BT_open_AlmaCam.TabIndex = 0;
+            this.BT_open_AlmaCam.Text = "Open almacam on selected server";
+            this.BT_open_AlmaCam.UseVisualStyleBackColor = true;
+            this.BT_open_AlmaCam.Click += new System.EventHandler(this.BT_open_AlmaCam_Click);
+            // 
+            // LB_Server_List
+            // 
+            this.LB_Server_List.FormattingEnabled = true;
+            this.LB_Server_List.Location = new System.Drawing.Point(12, 12);
+            this.LB_Server_List.Name = "LB_Server_List";
+            this.LB_Server_List.Size = new System.Drawing.Size(503, 21);
+            this.LB_Server_List.TabIndex = 1;
+            // 
+            // BT_open_WPM
+            // 
+            this.BT_open_WPM.Location = new System.Drawing.Point(12, 68);
+            this.BT_open_WPM.Name = "BT_open_WPM";
+            this.BT_open_WPM.Size = new System.Drawing.Size(503, 23);
+            this.BT_open_WPM.TabIndex = 0;
+            this.BT_open_WPM.Text = "Open WPM";
+            this.BT_open_WPM.Click += new System.EventHandler(this.BT_open_WPM_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(527, 165);
+            this.Controls.Add(this.BT_open_WPM);
+            this.Controls.Add(this.LB_Server_List);
+            this.Controls.Add(this.BT_open_AlmaCam);
+            this.Name = "Form1";
+            this.Text = "ShotCut";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BT_open_AlmaCam;
+        private System.Windows.Forms.ComboBox LB_Server_List;
+        private System.Windows.Forms.Button BT_open_WPM;
     }
 }
 
--- AlmaCamShortcut/Form1.cs
+++ AlmaCamShortcut/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,121 @@ namespace AlmaCamShortcut
         public Form1()
         {
             InitializeComponent();
+            List<string> shortcutslist = Import_Tools.GetShortcutList();
+            this.LB_Server_List.DataSource = shortcutslist;
         }
+
+        private void BT_open_WPM_Click(object sender, EventArgs e)
+        {
+            string almacamPath = Import_Tools.GetAlmaCamPath();
+
+            Process.Start(@almacamPath + "\\wpm.ini");
+        }
+
+        private void BT_open_AlmaCam_Click(object sender, EventArgs e)
+        {
+
+            string almacamPath = Import_Tools.GetAlmaCamPath();
+
+            Import_Tools.serverSubstitute(this.LB_Server_List.SelectedItem.ToString());
+
+            Process.Start(almacamPath + "\\Wpm.Implement.Editor.exe");
+
+        }
+    }
+
+
+    static class Import_Tools
+    {
+        public static string GetAlmaCamPath()
+        {  // a faire
+            //get infos from registry Ordinateur\HKEY_CURRENT_USER\Software\Alma\ACSE\Settings
+            return @"C:\AlmaCAM\Bin";
+        }
+        public static List<string> GetShortcutList()
+        {
+            List<string> servers = new List<string>();
+            string wpmpath= Import_Tools.GetAlmaCamPath()+"\\wpm.ini";
+            using (StreamReader wpmini = new StreamReader(@wpmpath))
+            {
+                while (!wpmini.EndOfStream)
+                {
+                    string line = wpmini.ReadLine();
+                    if (line.Contains("##SHORTCUT:")){
+                        
+                      
+                        if (line.Split(':').Count() == 4) {
+                            servers.Add(line.Split(':')[1].Trim()+":"+ line.Split(':')[2].Trim());
+                        }
+                    }
+                        
+                }
+
+            }
+
+                return servers;
+        }
+        public static void serverSubstitute(string selected_server)
+        {
+            //utiliser les ini /// mùais c'est chiant de le refaire.. prendre ma lib si besoin
+          
+            string wpmpath = Import_Tools.GetAlmaCamPath() + "\\wpm.ini";
+            string newserver = selected_server.Split(':')[0];
+            string portnumber = selected_server.Split(':')[1];
+            if (File.Exists(wpmpath + ".tmp"))
+            {
+                File.Delete(wpmpath+".tmp");
+            }
+            if (File.Exists(wpmpath))
+            {
+                File.Copy(wpmpath, wpmpath + ".tmp");
+                File.Delete(wpmpath);
+            }
+
+            using (var input = File.OpenText(wpmpath + ".tmp"))
+                
+            using (var output = new StreamWriter(wpmpath))
+            {
+                string line;
+                while (null != (line = input.ReadLine()))
+                {
+                    // optionally modify line.
+
+                    if (line.StartsWith("Name="))
+                    {
+
+
+                        if (line.Split('=').Count() == 2)
+                        {
+                            output.WriteLine("Name=" + newserver);
+
+                        }
+
+
+
+                    }
+                    else if(line.StartsWith("Port="))
+                    {
+                        output.WriteLine("Port=" + portnumber);
+                    }
+                    else
+                    {
+
+                        output.WriteLine(line.Trim());
+                    }
+
+                }
+
+            }
+
+           
+
+
+
+
+
+        }
+        
+
     }
 }
